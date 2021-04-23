6ac1e017cd5e22e1fda684ba6290bd9d20018909
cavalcant3 <jmatheusoc269@gmail.com>
2021-04-23 04:45:30 -0300

    criação da tela de confirmação

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+
+} from 'react-native';
+
+import { Button } from '../pages/components/Button';
+
+import colors from '../pages/styles/colors';
+import fonts from '../pages/styles/fonts';
+
+export function Confirmation(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>
+                  😁
+        </Text>
+
+          <Text style={styles.title}>
+              Prontinho
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Agora vamos começar a cuidar de suas
+            plantinhas com muito cuidado.
+          </Text>
+
+            <View style={styles.footer}>
+          
+          <Button title='Começar' />
+
+        </View>
+      </View>
+   
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'space-around',
+    
+  },
+  content:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+    width:'100%',
+    padding: 30,
+  },
+  title:{
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    textAlign: 'center',
+    color: colors.heading,
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle:{
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingVertical: 10,
+    color: colors.heading,
+  },
+  emoji:{
+    fontSize: 78,
+  },
+  footer:{
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20,
+
+  }
+})
